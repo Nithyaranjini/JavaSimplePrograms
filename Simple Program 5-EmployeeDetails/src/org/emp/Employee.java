2a7eebed4748f4e98b3625dbe39bf1ea11da853b
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 14:19:12 +0530

    commit

---
+++ Simple Program 5-EmployeeDetails/src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Employee
+{
+	private void empId()
+	{
+		System.out.println("tcs12345678");
+	}
+	private void empName() {
+		System.out.println("sree");
+	}
+	private void empDob() {
+		System.out.println("14/11/2000");}
+	private void empPhone() {
+	System.out.println("7512346789");}
+	private void empEmail() {
+		System.out.println("sri.sree123@gmail.com");}
+	private void empAddress() {
+		System.out.println("anna nagar chennai");}
+	
+public static void main(String[] args) {
+	Employee nithya = new Employee();
+	nithya.empId();
+	nithya.empName();
+	nithya.empDob();
+	nithya.empPhone();
+	nithya.empEmail();
+	nithya.empAddress();}
+	
+}
+	
+
+
+
+
+
