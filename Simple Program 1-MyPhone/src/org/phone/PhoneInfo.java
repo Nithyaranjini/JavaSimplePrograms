8c73393d94ec11c65805ad522f47a6348ac06fdc
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 16:24:15 +0530

    commit

---
+++ Simple Program 1-MyPhone/src/org/phone/PhoneInfo.java
@@ -0,0 +1,63 @@
+package org.phone;
+
+
+public class PhoneInfo {
+	private void phoneName() {
+		System.out.println("Oppo A9");}
+	private void phoneImeiNumber() {
+	    System.out.println("IMEI1:1234567891012345");}
+	private void camera() {
+		System.out.println("16 MP");
+		System.out.println("Rear Sensor:48MP & 8MP");}
+    private void Storage() {
+		System.out.println("128GB");}
+    private void osName() {
+    	System.out.println("ColorOS 6.0.1,Based on Android 9");}
+    private void dimensions() {
+    	System.out.println("Height:16.3cm/163.6mm");
+    	System.out.println("Width:7.56cm/75.6mm");
+    	System.out.println("Thickness:0.91cm");}
+    private void phoneSensors() {
+	     System.out.println("Geomagnetic,Light,Gyro,G");}
+    private void colour() {
+		 System.out.println("Green,Blue,Black");}
+		
+    public static void main(String[] args) {
+	     PhoneInfo phonedetails = new PhoneInfo();
+	     phonedetails.phoneName();
+	     phonedetails.phoneImeiNumber();
+	     phonedetails.camera();
+	     phonedetails.Storage();
+	     phonedetails.osName();
+	     phonedetails.dimensions();
+	     phonedetails.phoneSensors();
+	     phonedetails.colour();
+    }
+}
+	     
+	     
+	    
+	
+	     
+	
+	     
+
+		
+	
+
+	
+    	
+	
+
+	
+		
+
+		
+
+	
+	
+
+	
+	
+
+
