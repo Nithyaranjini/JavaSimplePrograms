b5a861c070cc93cd87decdc736a0691d88f45687
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 16:26:47 +0530

    commit

---
+++ Simple Program 2-LanguageDetails/src/org/lang/LanguageInfo.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class LanguageInfo {
+public void tamilLanguage() {
+		System.out.println("Tamil is my first Language");}
+public void englishLanguage() {
+	    System.out.println("English is my second Language");}
+public void hindiLanguage() {
+	    System.out.println("Hindi is my third Language");}
+	
+}
+
+
+
+
+
+
+
+	
+
+
---
+++ Simple Program 2-LanguageDetails/src/org/lang/StateDetails.java
@@ -0,0 +1,31 @@
+package org.lang;
+
+public class StateDetails {
+	private void southIndia() {
+		System.out.println("SouthIndia is my fav place");}
+	private void northIndia() {
+		System.out.println("Delhi is the part of NorthIndia");}
+	public static void main(String[] args) {
+		StateDetails c  = new StateDetails();
+		c.southIndia();
+		c.northIndia();
+		LanguageInfo e = new LanguageInfo();
+		e.tamilLanguage();
+		e.englishLanguage();
+		e.hindiLanguage();}
+}
+		
+				
+		
+		
+				
+		
+	
+	
+	
+
+	
+
+	
+
+
