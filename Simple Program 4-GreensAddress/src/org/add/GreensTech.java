b7ea623d95e4b1865e9948e589ad4e095e7df738
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 15:53:54 +0530

    commit

---
+++ Simple Program 4-GreensAddress/src/org/add/GreensTech.java
@@ -0,0 +1,42 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+	System.out.println("Software Training and Placement New Batch,Thoraipakkam");}
+	private void greensAdayar() {
+		System.out.println("Software Training and Placement is not started,Adayar");}
+	private void greensTambaram() {
+		System.out.println("Software Training and Placement Sep Batch,Tambaram");}
+	private void greensVelacherry() {
+		System.out.println("Software Training and Placement Oct Batch,Velacherry");}
+	private void greensAnnaNagar() {
+		System.out.println("Software Training and Placement is started,AnnaNagar");}
+	
+	public static void main(String[] args) {
+		GreensTech n = new GreensTech();
+		n.greensOmr();
+		n.greensAdayar();
+		n.greensTambaram();
+		n.greensVelacherry();
+		n.greensAnnaNagar();
+		
+		}
+	
+		
+
+	
+
+	}
+
+	
+	
+		
+
+
+	
+	
+
+	
+
+
+
